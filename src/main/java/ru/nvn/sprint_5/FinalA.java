ea1e32fdd78528d0f05ea7dfd2c553588fe5ad73
nikisV <nikisvedenyapin@gmail.com>
2023-04-17 15:22:04 +0300

    feat

--- src/main/java/ru/nvn/sprint_5/FinalA.java
+++ src/main/java/ru/nvn/sprint_5/FinalA.java
@@ -1,82 +1,119 @@
+//https://contest.yandex.ru/contest/24810/run-report/85924450/
+
 package ru.nvn.sprint_5;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
 
 public class FinalA {
 
   /**
-   * -- ПРИНЦИП РАБОТЫ -- 1.Из исходного массива строим бинарную кучу, где каждый элемент является
-   * родителем для своих дочерних элементов и имеет значение, большее или равное, чем у своих детей.
-   * 2.После построения кучи, меняем местами корневой элемент (максимальный) с последним элементом в
-   * массиве. 3.Уменьшаем размер кучи на 1, и вызываем процедуру "просеивания" корня (т.е.
-   * соблюдения правил бинарной кучи). 4.Повторяем шаги 2 и 3 до тех пор, пока в куче не останется 1
-   * элемент. 5.Отсортированный массив будет получен в обратном порядке - от последнего к первому.
+   * -- ПРИНЦИП РАБОТЫ -- 1.Сначала производится построение неубывающей пирамиды из элементов
+   * массива, начиная с последнего элемента и заканчивая первым (вниз-вверх). 2.Затем извлекается
+   * максимальный элемент из пирамиды и помещается в конец массива. Затем производится
+   * восстановление свойств пирамиды путем спуска элемента из вершины вниз. 3.Шаг 2 повторяется до
+   * тех пор, пока пирамида не опустеет.
    *
-   * <p>-- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ -- 1. Построение кучи гарантирует, что максимальный элемент
-   * находится в корне. 2. Меняем местами максимальный элемент с последним в массиве, что
-   * гарантирует, что максимальный элемент будет находиться в конце массива. 3. После этого
-   * производим просеивание корня, чтобы восстановить свойства бинарной кучи. 4. Повторяем шаги 2 и
-   * 3 до тех пор, пока в куче не останется 1 элемент. 5. Корректность алгоритма доказана тем, что
-   * он сначала формирует корректную бинарную кучу, а затем на каждом шаге перемещает максимальный
-   * элемент на свое место в отсортированном массиве.
+   * <p>-- ДОКАЗАТЕЛЬСТВО КОРРЕКТНОСТИ -- 1. Построение пирамиды происходит корректно, т.к. в каждом
+   * узле пирамиды находится максимум из своих потомков. 2. После извлечения максимального элемента
+   * из пирамиды и его перемещения в конец массива, новая пирамида будет удовлетворять свойствам
+   * неубывающей последовательности. 3. Так как каждый из n элементов массива извлекается из
+   * пирамиды и переставляется в конец массива, то после n таких операций массив будет отсортирован.
    *
-   * <p>-- ВРЕМЕННАЯ СЛОЖНОСТЬ --  O(nlog n), где n - число элементов
-   * в массиве. Построение бинарной кучи требует O(n) операций, а каждое просеивание занимает O(log
-   * n) операций. Всего нужно выполнить n-1 операций для построения отсортированного массива, что
-   * дает общую сложность O(nlog n).
+   * <p>-- ВРЕМЕННАЯ СЛОЖНОСТЬ -- O(n log n), так как построение пирамиды занимает O(n) времени, а
+   * каждое извлечение максимального элемента и восстановление свойств пирамиды занимает O(log n)
+   * времени.
    *
-   * <p>-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ --O(1), т.к. алгоритм работает с исходным массивом,
-   * без создания дополнительных структур данных.
-   *
+   * <p>-- ПРОСТРАНСТВЕННАЯ СЛОЖНОСТЬ -- Пространственная сложность алгоритма будет O(1), так как
+   * все операции производятся над исходным массивом без использования дополнительной памяти.
    */
   public static void main(String[] args) throws IOException {
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
     int n = Integer.parseInt(reader.readLine());
-
     Participant[] participants = new Participant[n];
     for (int i = 0; i < n; i++) {
       String[] input = reader.readLine().split(" ");
-      String name = input[0];
-      int tasks = Integer.parseInt(input[1]);
+      String login = input[0];
+      int solved = Integer.parseInt(input[1]);
       int penalty = Integer.parseInt(input[2]);
-      participants[i] = new Participant(name, tasks, penalty);
+      participants[i] = new Participant(login, solved, penalty);
     }
 
-    Arrays.sort(participants, new ParticipantComparator());
+    heapSort(participants);
 
     for (Participant participant : participants) {
-      System.out.println(participant.name);
+      System.out.println(participant.getLogin());
     }
   }
 
-  static class Participant {
-    String name;
-    int tasks;
-    int penalty;
+  private static void heapSort(Participant[] participants) {
+    buildMaxHeap(participants);
 
-    public Participant(String name, int tasks, int penalty) {
-      this.name = name;
-      this.tasks = tasks;
-      this.penalty = penalty;
+    for (int i = participants.length - 1; i > 0; i--) {
+      swap(participants, 0, i);
+      maxHeapify(participants, 0, i);
     }
   }
 
-  static class ParticipantComparator implements Comparator<Participant> {
-
-    @Override
-    public int compare(Participant p1, Participant p2) {
-      if (p1.tasks != p2.tasks) {
-        return Integer.compare(p2.tasks, p1.tasks);
-      } else if (p1.penalty != p2.penalty) {
-        return Integer.compare(p1.penalty, p2.penalty);
-      } else {
-        return p1.name.compareTo(p2.name);
-      }
+  private static void buildMaxHeap(Participant[] participants) {
+    for (int i = participants.length / 2 - 1; i >= 0; i--) {
+      maxHeapify(participants, i, participants.length);
     }
+  }
+
+  private static void maxHeapify(Participant[] participants, int i, int heapSize) {
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
+    int largest = i;
+
+    if (l < heapSize && participants[l].compareTo(participants[largest]) > 0) {
+      largest = l;
+    }
+
+    if (r < heapSize && participants[r].compareTo(participants[largest]) > 0) {
+      largest = r;
+    }
+
+    if (largest != i) {
+      swap(participants, i, largest);
+      maxHeapify(participants, largest, heapSize);
+    }
+  }
+
+  private static void swap(Participant[] participants, int i, int j) {
+    Participant temp = participants[i];
+    participants[i] = participants[j];
+    participants[j] = temp;
+  }
+}
+
+class Participant implements Comparable<Participant> {
+  private String login;
+  private int solved;
+  private int penalty;
+
+  public Participant(String login, int solved, int penalty) {
+    this.login = login;
+    this.solved = solved;
+    this.penalty = penalty;
+  }
+
+  public String getLogin() {
+    return login;
+  }
+
+  @Override
+  public int compareTo(Participant o) {
+    if (this.solved != o.solved) {
+      return Integer.compare(o.solved, this.solved);
+    }
+
+    if (this.penalty != o.penalty) {
+      return Integer.compare(this.penalty, o.penalty);
+    }
+
+    return this.login.compareTo(o.login);
   }
 }
