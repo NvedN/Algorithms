2280ddda30de32c500d662a4054464de94c9fc07
nikisV <nikisvedenyapin@gmail.com>
2023-11-07 16:52:42 +0300

    fix: problem 3

--- src/main/java/ru/nvn/leetcode/LengthOfLongestSubstring.java
+++ src/main/java/ru/nvn/leetcode/LengthOfLongestSubstring.java
@@ -7,15 +7,25 @@ public class LengthOfLongestSubstring {
 
   public static int lengthOfLongestSubstring(String s) {
 
-    Set<Character> set = new HashSet<>();
-    int max = 0;
-    int left = 0;
-    for (int right = 0; right < s.length(); right++) {
-      while (!set.add(s.charAt(right))) {
-        set.remove(s.charAt(left++));
-      }
-      max = Math.max(max, right - left + 1);
+    if (s == null || s.length() == 0) {
+      return 0;
     }
-    return max;
+
+    int maxLength = 0;
+    int left = 0;
+    int right = 0;
+    Set<Character> charSet = new HashSet<>();
+    while (right < s.length()) {
+      if (!charSet.contains(s.charAt(right))) {
+        charSet.add(s.charAt(right));
+        maxLength = Math.max(maxLength, right - left + 1);
+        right++;
+      } else {
+        charSet.remove(s.charAt(left));
+        left++;
+      }
+    }
+
+    return maxLength;
   }
 }
